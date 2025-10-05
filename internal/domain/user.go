c140dad9a526807051e898f2f7af2f518f8e34ae
kertoflenida <sskaverinass@gmail.com>
2025-10-05 14:35:14 +0300

    Domain user

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,11 +1,82 @@
 package domain
 
-type BaseUser struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+	"regexp"
+)
+
+type User struct {
+	id int64
+	userName string
+	email string
+	hashedPassword string
 }
 
-type RegisterUser struct {
-	BaseUser
-	UserName string `json:"user_name"`
+var allowedSymbols = regexp.MustCompile(`^[a-zA-Z0-9._]+$`)
+var allowedEmail = regexp.MustCompile(`^[a-zA-Z0-9._]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$`);
+
+func NewUser(userName, email, password string) (*User, error){
+	if len(userName)<3 || len(userName)>20{
+		return nil, errors.New("Имя пользователя должно быть не меньше 3-х и не больше 20-ти символов")
+	}
+
+	if !allowedSymbols.MatchString(userName) {
+		return nil, errors.New("Имя пользователя содержит недопустимые значения")
+	}
+
+	if !allowedEmail.MatchString(email) {
+		return nil, errors.New("Недопустимое имя Email")
+	}
+	
+	if len(password) < 8 {
+		return nil, errors.New("Пароль менее 8-ми символов")
+	}
+	
+	if len(password) > 50 {
+		return nil, errors.New("Пароль больше 50-ти символов")
+	}
+	
+	if !allowedSymbols.MatchString(password){
+		return nil, errors.New("Недопустимые значения")
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		userName: userName,
+		email: email,
+		hashedPassword: string(hashedPassword),
+	}, nil
+}
+
+func (u *User) ComparePasswords(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.hashedPassword),([]byte(password)))
+	return err == nil
+}
+
+func LoginUser(email, password string) (*User, error){
+	if !allowedEmail.MatchString(email){
+		return nil, errors.New("Недопустимое имя Email")
+	}
+	
+	if len(password) < 8{
+		return nil, errors.New("Пароль менее 8-ми символов")
+	}
+	
+	if len(password) > 50 {
+		return nil, errors.New("Пароль больше 50-ти символов")
+	}
+	
+	if !allowedSymbols.MatchString(password){
+		return nil, errors.New("Недопустимые значения")
+	}
+	
+	return &User{
+		email: email,
+		hashedPassword: password,
+	}, nil
 }
--- internal/usecase/auth_usecase.go
+++ internal/usecase/auth_usecase.go
@@ -1,1 +1,43 @@
 package usecase
+
+import(
+	"2025_2_404/internal/domain"
+	"errors"
+)
+
+type BaseUser struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type RegisterUser struct {
+	BaseUser
+	UserName string `json:"user_name"`
+}
+
+type UserUseCase struct {
+	userRepo domain.UserRepository
+}
+
+// dto - data transport object 
+
+func (uc *UserUseCase) Register(dto RegisterUser) (*domain.User, error) {
+	user, err := domain.NewUser(dto.UserName, dto.Email, dto.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (uc *UserUseCase) Login(dto BaseUser) (*domain.User, error) {
+	user, err := domain.LoginUser(dto.Email, dto.Password)
+	if err != nil {
+		return nil, err
+	}
+	if !user.ComparePasswords(dto.Password) {
+		return nil, errors.New("Неверный пароль")
+	}
+
+	return user, nil
+}
