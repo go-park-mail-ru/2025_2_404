7f821a3b68fd5a864ddf5de74cfe2ee491d1374e
ShadowNair <borodin_glebka@list.ru>
2025-10-05 22:20:14 +0300

    1part3

---
+++ db/migrations/000000_init.sql
@@ -0,0 +1,103 @@
+
+-- Создание таблицы client
+CREATE TABLE IF NOT EXISTS client (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL CHECK (
+		length(name) >= 4 AND length(name) <= 50
+	),
+	email TEXT UNIQUE NOT NULL CHECK (
+		length(email) >= 5 AND length(email) <= 100
+	),
+	password_hash TEXT NOT NULL CHECK (
+		length(password_hash) >= 8 AND length(password_hash) <= 50
+	),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS client_wallet (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	client_id INTEGER REFERENCES client(id) ON DELETE CASCADE,
+	balance INT NOT NULL DEFAULT 0 CHECK (balance >= 0),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS wallet_top_up (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	client_id INTEGER REFERENCES client(id) ON DELETE CASCADE,
+	amount INT NOT NULL CHECK (amount > 0),
+    payment_method TEXT NOT NULL CHECK (
+		length(payment_method) >= 1 AND length(payment_method) <= 40
+	),
+    status TEXT NOT NULL CHECK (
+		length(status) >= 1 AND length(status) <= 40
+	),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS notifications (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	client_id INTEGER REFERENCES client(id) ON DELETE CASCADE,
+	notification_text TEXT NOT NULL CHECK (
+		length(notification_text) >= 1 AND length(notification_text) <= 200
+	),
+    type TEXT NOT NULL CHECK (
+		length(type) >= 1 AND length(type) <= 40
+	),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS platform (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	platform_name TEXT NOT NULL CHECK (
+		length(platform_name) >= 1 AND length(platform_name) <= 100
+	),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS ad (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	creator_id INTEGER REFERENCES client(id) ON DELETE CASCADE,
+	title TEXT NOT NULL CHECK (
+		length(title) >= 1 AND length(title) <= 40
+	),
+	content TEXT NOT NULL CHECK (
+		length(content) >= 1 AND length(content) <= 200
+	),
+    img_bin BYTEA,
+    target_url TEXT NOT NULL CHECK (
+		length(target_url) >= 1 AND length(target_url) <= 200
+	),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS ad_details (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	ad_id INTEGER REFERENCES ad(id) ON DELETE CASCADE,
+	platform_id INTEGER REFERENCES platform(id) ON DELETE CASCADE,
+    status TEXT NOT NULL CHECK (
+		length(status) >= 1 AND length(status) <= 40
+	),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS statistics (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	ad_details_id INTEGER REFERENCES ad_details(id) ON DELETE CASCADE,
+	clicks INT NOT NULL DEFAULT 0 CHECK (clicks >= 0),
+	impressions INT NOT NULL DEFAULT 0 CHECK (impressions >= 0),
+    conversion DECIMAL(5, 2) NOT NULL DEFAULT 0 CHECK (conversion >= 0),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS ad_platform (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	ad_id INTEGER REFERENCES ad(id) ON DELETE CASCADE,
+	platform_id INTEGER REFERENCES platform(id) ON DELETE CASCADE,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
