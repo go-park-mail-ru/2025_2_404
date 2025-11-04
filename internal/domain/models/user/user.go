8deedbf78d9e41864139022ea46815f8bc3e041e
ShadowNair <borodin_glebka@list.ru>
2025-11-04 15:59:30 +0300

    json

--- internal/domain/models/user/user.go
+++ internal/domain/models/user/user.go
@@ -12,7 +12,7 @@ type User struct {
 	ID          ID	`json:"id"`
 	UserName    string	`json:"user_name"`
 	Email      string	`json:"email"`
-	ImagePath string `json:image_path`
+	ImagePath string `json:"image_path"`
 	HashedPassword string	`json:"password"`
 }
 
