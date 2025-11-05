0fa142643d4d6c94bf5ace7aa4f88afbb62f58be
ShadowNair <borodin_glebka@list.ru>
2025-11-05 13:09:38 +0300

    CORS

--- internal/delivery/http/middleware/middleware.go
+++ internal/delivery/http/middleware/middleware.go
@@ -59,6 +59,11 @@ func (u *Middleware) Peflite(next http.Handler) http.Handler {
         "http://127.0.0.1:8000":        true,
         "http://89.208.230.119:8000":   true,
 		"http://89.208.230.119:3000":   true,
+		"http://0.0.0.0:3000":   true,
+		"http://0.0.0.0:8000":   true,
+		"http://89.208.230.119:8080":   true,
+		"http://0.0.0.1:3000":   true,
+		"http://0.0.0.1:8000":   true,
     }
 
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
