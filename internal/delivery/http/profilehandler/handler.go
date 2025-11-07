a2dc1ecb2611371b04d71679798a4d1849ec078d
ShadowNair <borodin_glebka@list.ru>
2025-11-07 18:24:51 +0300

    +img

--- internal/delivery/http/profilehandler/handler.go
+++ internal/delivery/http/profilehandler/handler.go
@@ -54,7 +54,7 @@ func (h *UserHandler) UpdateHandler (w http.ResponseWriter, r *http.Request){
 	client.Phone = r.FormValue("phone_number")
 	
 	
-	imgFile, header, err := r.FormFile("image")
+	imgFile, header, err := r.FormFile("img")
 	if err != nil && err != http.ErrMissingFile{
 		http.Error(w, "Invalid file", http.StatusBadRequest)
 		return
