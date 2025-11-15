9be0e2b5a2f0931eb6b4e12be987e3d39a49e838
kertoflenida <sskaverinass@gmail.com>
2025-11-15 13:46:34 +0300

    added deletye

--- internal/repository/postgres/support/postgres.go
+++ internal/repository/postgres/support/postgres.go
@@ -13,6 +13,8 @@ const(
     sqlTextForSelectSupport = "SELECT id, client_id, sup_status, category, sup_description, img_path, contact_name, contact_email FROM support WHERE id = $1 AND client.id = $2"
 	sqlTextForUpdateSupport = "UPDATE support SET sup_status = $1, category = $2, sup_description = $3, img_path = $4, contact_name = $5, contact_email = $6 WHERE id = $7 AND client_id = $8"
 	sqlTextForAllSupport = "SELECT id, sup_status, category, sup_description, img_path, contact_name, contact_email FROM support WHERE client_id = $1"
+	sqlTextForDeleteSupport = "DELETE FROM support WHERE id = $1"
+
 )
 
 type DB struct{
@@ -105,3 +107,20 @@ func (r *DB) Update(ctx context.Context, sup modelsup.Support) error {
 	return nil
 }
 
+func (r *DB) Delete(ctx context.Context, supID int64) error {
+	
+	result, err := r.sql.ExecContext(ctx, sqlTextForDeleteSupport, supID)
+	if err != nil {
+		return fmt.Errorf("failed to delete ad: %w", err)
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("Failed to get a rows: %w", err)
+	}
+	if rowsAffected == 0 {
+		return fmt.Errorf("Sup with ID %d not found", supID)
+	}
+	fmt.Printf("Пользователь с ID %d успешно удален. Затронуто строк: %d", supID, rowsAffected)
+	return nil
+}
