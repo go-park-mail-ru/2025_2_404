0a944cb6e3d985373db9d840b93b8a13696d2d52
kertoflenida <sskaverinass@gmail.com>
2025-11-15 13:43:54 +0300

    postgress

--- internal/domain/models/support/support.go
+++ internal/domain/models/support/support.go
@@ -4,16 +4,16 @@ import	(
 	modeluser "2025_2_404/internal/domain/models/user"
 )
 
-type ID int
+type ID int64
 
 type Support struct {
-	ID ID `json:support_id`
-	UserID modeluser.ID `json:user_id`
-	Status string `json:status`
-	Category string `json:category`
-	Description string `json:description`
-	ImagePath string `json:image_path`
-	ContactName string `json:contact_name`
-	ContactEmail string `json:contact_email`
+	ID ID `json:"support_id"`
+	UserID modeluser.ID `json:"user_id"`
+	Status string `json:"status"`
+	Category string `json:"category"`
+	Description string `json:"description"`
+	ImagePath string `json:"image_path"`
+	ContactName string `json:"contact_name"`
+	ContactEmail string `json:"contact_email"`
 }
 
--- internal/repository/postgres/support/postgres.go
+++ internal/repository/postgres/support/postgres.go
@@ -5,11 +5,14 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"strings"
+
 )
 
 const(
-    sqlTextForInsertSupport = "INSERT INTO support (client_id, sup_status, category, sup_description, contact_name, contact_email) VALUES ($1, $2, $3, $4, $5) RETURNING id"
+    sqlTextForInsertSupport = "INSERT INTO support (client_id, sup_status, category, sup_description, img_path, contact_name, contact_email) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id"
+    sqlTextForSelectSupport = "SELECT id, client_id, sup_status, category, sup_description, img_path, contact_name, contact_email FROM support WHERE id = $1 AND client.id = $2"
+	sqlTextForUpdateSupport = "UPDATE support SET sup_status = $1, category = $2, sup_description = $3, img_path = $4, contact_name = $5, contact_email = $6 WHERE id = $7 AND client_id = $8"
+	sqlTextForAllSupport = "SELECT id, sup_status, category, sup_description, img_path, contact_name, contact_email FROM support WHERE client_id = $1"
 )
 
 type DB struct{
@@ -23,7 +26,7 @@ func New(sql *sql.DB) *DB{
 }
 
 func (r *DB) Create(ctx context.Context, sup modelsup.Support) (error) {
-	res, err := r.sql.ExecContext(ctx, sqlTextForInsertSupport, )
+	res, err := r.sql.ExecContext(ctx, sqlTextForInsertSupport, sup.UserID, sup.Status, sup.Category, sup.Description, sup.ImagePath, sup.ContactName, sup.ContactEmail)
 	if err != nil {
 		return fmt.Errorf("failed to create ad: %w", err)
 	}
@@ -33,8 +36,72 @@ func (r *DB) Create(ctx context.Context, sup modelsup.Support) (error) {
 		return fmt.Errorf("failed to get rows affected: %w", err)
 	}
 	if rowsAffected == 0 {
-		return fmt.Errorf("ad with id %v not found", ad.ID)
+		return fmt.Errorf("ad with id %v not found", sup.ID)
 	}
 
 	return nil
 }
+
+func (r *DB) GetOneSup(ctx context.Context, supID int64) (modelsup.Support, error) {
+	var supInfo modelsup.Support
+	row := r.sql.QueryRowContext(ctx, sqlTextForSelectSupport, supID)
+
+	err :=  row.Scan(
+		&supInfo.ID,
+		&supInfo.UserID,
+		&supInfo.Status,
+		&supInfo.Category,
+		&supInfo.Description,
+		&supInfo.ImagePath,
+		&supInfo.ContactName,
+		&supInfo.ContactEmail,
+	)
+
+	if err != nil {
+		return modelsup.Support{}, fmt.Errorf("failed to find an ad: %w", err)
+	}
+
+	return supInfo, nil
+}
+
+func (r *DB) GetAllSups(ctx context.Context, superID int64) ([]modelsup.Support, error) {
+	rows, err := r.sql.QueryContext(ctx, sqlTextForAllSupport, superID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to query supports by super user ID: %w", err)
+	}
+	defer rows.Close()
+
+	var support []modelsup.Support
+	for rows.Next() {
+		var sup modelsup.Support
+		err := rows.Scan(&sup.ID, &sup.Status, &sup.Category, &sup.Description, &sup.ImagePath, &sup.ContactName, &sup.ContactEmail)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan ad: %w", err)
+		}
+		support = append(support, sup)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("rows iteration error: %w", err)
+	}
+
+	return support, nil
+}
+
+func (r *DB) Update(ctx context.Context, sup modelsup.Support) error {
+	res, err := r.sql.ExecContext(ctx, sqlTextForUpdateSupport, sup.Status, sup.Category, sup.Description, sup.ImagePath, sup.ContactName, sup.ContactEmail, sup.ID, sup.UserID)
+	if err != nil {
+		return fmt.Errorf("failed to update ad: %w", err)
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("failed to get rows affected: %w", err)
+	}
+	if rowsAffected == 0 {
+		return fmt.Errorf("sup with id %v and client_id %v not found", sup.ID, sup.UserID)
+	}
+
+	return nil
+}
+
