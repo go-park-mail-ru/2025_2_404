db9d0931664a2c5887e39a54cb3f930e8836db49
ShadowNair <borodin_glebka@list.ru>
2025-12-20 05:20:54 +0300

    Settings balance

--- internal/delivery/http/profile.go
+++ internal/delivery/http/profile.go
@@ -296,6 +296,7 @@ func (h *ProfileHandler) SubtractBalance(w http.ResponseWriter, r *http.Request)
 
 	req := &pbProfile.SubtractBalanceRequest{
 		SubAmount: jsonReq.SubtractAmount,
+		Type: "subtract_balance",
 	}
 
 	resp, err := h.client.SubtractBalance(ctx, req)
